8b7067034c1c2ad531411064603f3c873fada7eb
a7med2003 <54036248+a7med2003@users.noreply.github.com>
2019-08-12 19:45:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,19 +4,22 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
+var prefix = "-";
 
 
+client.on("message", message => {
 
-
-
-
-
-// CODES HERE
-
-
-
-
-
+            if (message.content.startsWith(prefix + "bc")) {///كود البرودكاست
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
 
 
 // THIS  MUST  BE  THIS  WAY
